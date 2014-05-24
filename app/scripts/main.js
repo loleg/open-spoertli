45481b889a1d24d882a6714b3d2ee9d0b6bd6ba8
Oleg Lavrovsky <oleg@utou.ch>
2014-05-24 16:45:31 +0200

    Working browser

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,31 +1,39 @@
 
 // Wrapping in nv.addGraph allows for '0 timeout render', stores rendered charts in nv.graphs, and may do more in the future... it's NOT required
 var chart;
-nv.addGraph(function() {  
 
-   chart = nv.models.cumulativeLineChart()
-             .x(function(d) { return d[0] })
-             .y(function(d) { return d[1]/100 })
-             .color(d3.scale.category10().range())
-             .clipVoronoi(false);
+function redraw() {
+  nv.addGraph(function() {  
 
-   chart.xAxis
-      .tickFormat(function(d) {
-          return d //d3.time.format('%x')(new Date(d))
-        });
+     chart = nv.models.cumulativeLineChart()
+               .x(function(d) { return d[0] })
+               .y(function(d) { return d[1]/100 })
+               .color(d3.scale.category10().range())
+               .useInteractiveGuideline(true);
 
-  chart.yAxis
-      .tickFormat(d3.format(',.1%'));
+     chart.xAxis
+        .tickFormat(function(d) {
+            return d; //d3.time.format('%x')(new Date(d))
+          });
 
+    chart.yAxis
+        .tickFormat(d3.format(',.1%'));
 
-  //TODO: Figure out a good way to do this automatically
-  nv.utils.windowResize(chart.update);
-  //nv.utils.windowResize(function() { d3.select('#chart1 svg').call(chart) });
+    d3.select('#chart1 svg')
+          .datum(theData)
+          .transition()
+          .duration(0)
+          .call(chart);
 
-  chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+    //TODO: Figure out a good way to do this automatically
+    nv.utils.windowResize(chart.update);
+    //nv.utils.windowResize(function() { d3.select('#chart1 svg').call(chart) });
 
-  return chart;
-});
+    chart.dispatch.on('stateChange', function(e) { nv.log('New State:', JSON.stringify(e)); });
+
+    return chart;
+  });
+}
 
 // Get 'our' data set
 $.getJSON('data/J-S-2004-2012.json', function(data) {
@@ -48,6 +56,7 @@ $.getJSON('data/J-S-2004-2012.json', function(data) {
       });
     }
     $.each(sportInfos, function(i, v) {
+      if (typeof self[v] !== 'undefined')
         sportData[sa][i].values.push([self['Jahr'], self[v]]);
     });
   });
@@ -62,11 +71,12 @@ $.getJSON('data/J-S-2004-2012.json', function(data) {
   $('#sport-choose').click(function() { $('.sport-buttons').slideDown(); $(this).hide(); });
   $('#sport-dismiss').click(function() { $('.sport-buttons').slideUp(); $('#sport-choose').show();  });
   $('.sport-buttons .btn').click(function() { $('.sport-buttons').slideUp(); $('#sport-choose').show(); 
-    var data = sportData[$(this).attr('data-sport')];
-    console.log(data);
-    d3.select('#chart1 svg')
-      .datum(data)
-      .transition().duration(500)
-      .call(chart);
+    var t = $(this).attr('data-sport');
+    theData = sportData[t];
+    $('.sport-title').html(t);
+    //console.log(data);
+    redraw();
   });
 });
+
+var theData = null;
